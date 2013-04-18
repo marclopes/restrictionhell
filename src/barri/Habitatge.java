7cae3a24f2931ffe9541fd70b0988a85d52e725f
marclopez <lopex.linux@gmail.com>
2013-04-18 16:58:09 +0200

    habitatge amb enum

--- src/barri/Habitatge.java
+++ src/barri/Habitatge.java
@@ -6,12 +6,18 @@ public class Habitatge extends Edifici{
 	private int impost;
 	private int aparcament;
 	
+	public enum TipusHab {
+		PIS, APARELLADES, UNIFAMILIAR, MANSIO, BARRACA
+	};
+	
+	
+	
 	Habitatge(){
 		
 	}
 	
 	public Habitatge(int impost, int aparcament, String nom, int codi, int h, int capacitat, List<String> c)throws Exception{
-		super(nom, codi, ...);
+		super(nom, codi, h, capacitat, c);
 		super.tipusEd = TipusEd.HAB;
 		if(impost < 0) throw new Exception("Impost no pot ser negatiu.");
 		if(aparcament < 0) throw new Exception("Aparcament no pot ser negatiu.");
