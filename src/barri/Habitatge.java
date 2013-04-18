bf95d91aa1e8ad834dbe3f54f02c3da534f20083
marclopes <lopex.linux@gmail.com>
2013-04-18 18:48:23 +0300

    Update Habitatge.java
    cutreeee

--- src/barri/Habitatge.java
+++ src/barri/Habitatge.java
@@ -1,22 +1,23 @@
 package barri;
 import java.util.*;
 
-/**
-*
-* @author Edu
-*/
-
 public class Habitatge extends Edifici{
-	
+
 	private int impost;
 	private int aparcament;
+	
+	public enum TipusHab {
+		PIS, APARELLADES, UNIFAMILIAR, MANSIO, BARRACA
+	};
+	
+	
 	
 	Habitatge(){
 		
 	}
 	
 	public Habitatge(int impost, int aparcament, String nom, int codi, int h, int capacitat, List<String> c)throws Exception{
-		super(nom, codi, ...);
+		super(nom, codi, h, capacitat, c);
 		super.tipusEd = TipusEd.HAB;
 		if(impost < 0) throw new Exception("Impost no pot ser negatiu.");
 		if(aparcament < 0) throw new Exception("Aparcament no pot ser negatiu.");
