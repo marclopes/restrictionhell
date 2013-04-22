9d69b49a611f9a522571afb2b599fdcc6339c68f
Navax99 <navarro_635@hotmail.com>
2013-04-22 20:45:22 +0200

    Em carrego l'espai antic

--- src/barri/CtrDomGeneral.java
+++ src/barri/CtrDomGeneral.java
@@ -8,9 +8,49 @@ public class CtrDomGeneral {
   private CtrDomRestriccions ctrRestric;
   
   private CtrDomGeneral(){
+    ctrBarri = CtrBarriDom.GetInstancia();
+    ctrEdificis = CtrDomEdificis.GetInstancia();
+    ctrRestric = CtrDomRestriccions.GetInstancia();
   }
   public CtrDomGeneral GetInstancia() {
     if ( ctrDomini== null ) ctrDomini = new CtrBarriDom();
-    else return ctrDomini;
+    return ctrDomini;
   }
+  
+  public void CreaBarri(){
+  }
+  public void CrearRestriccio(){
+  }
+  public void CrearEdifici(){
+  }
+  public void ModificaBarri() {
+  }
+  public void ModificarEdifici(){
+  }
+  public void ModificarRestriccio(){
+  }
+  public void EliminarEdifici(){
+  }
+  public void EliminarBarri(){
+  }
+  public void EliminarRestriccio(){
+  }
+  public void CarregarCatalegEdifici(){
+  }
+  public void CarregarCatalegRestriccio(){
+  }
+  public void CarregarBarri(){ //binari barri
+  }
+  
+  public void AfegirEdifici(){//al barri
+  }
+  public void ImposarRestriccio(){
+  }
+  public void TreuEdifici(){
+  }
+  public void TreuRestriccio(){
+  }
+  public void GeneraBarri(){
+  }
+  
 }
