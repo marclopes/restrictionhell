694e6196e1abedac7cb07850c98a217652cfc004
Edu Ricou <eduricou@gmail.com>
2013-05-02 16:18:03 +0200

    javadoc restriccions

--- src/barri/RAlsada.java
+++ src/barri/RAlsada.java
@@ -5,7 +5,12 @@ public class RAlsada extends RestriccioBarris implements RCjtEd{
 	CjtEdificis ce;
 	int alsada;
 	
-
+        /**
+         * Crea una instancia de la restricció de Alçada.
+         * @param ID Identificador de la restricció.
+         * @param al Alçada màxima del barri
+         * @param ce Conjunt d'edificis sobre el que es comprovarà la restricció.
+         */
 	public RAlsada(int ID, int al, CjtEdificis ce) {
 		super(ID);
 		this.ce = ce;
@@ -14,9 +19,11 @@ public class RAlsada extends RestriccioBarris implements RCjtEd{
 		
 	}
 
-	
+	/**
+        * Comprova que es compleixi la restricció.
+        * @return cert si es compleix la restricció.
+        */
 	public boolean CompleixRes() {
-		//boolean comp = true;
 		for (int i = 0; i < ce.Tamany(); i++) {
 			if (ce.ObtenirEdifici(i).ConsultarH() > alsada) return false;
 		}
@@ -24,21 +31,32 @@ public class RAlsada extends RestriccioBarris implements RCjtEd{
 		return true;
 	}
 	
+        /**
+         * Consultora de l'alçada màxima del barri.
+         * @return L'alçada màxima del barri.
+         */
 	public int ConsultarAlsada() {
 		return alsada;
 	}
 	
+        /**
+         * Modifica l'alçada màxima del barri.
+         * @param a Nova alçada màxima del barri.
+         */
 	public void ModificarAlsada(int a) {
 		alsada = a;
 	}
 	
-	
+	/**
+         * Assigna un conjunt d'edificis a la restricció.
+         * @param ce Nou conjunt d'edificis.
+         */
 	public void AssignaCe(CjtEdificis ce) {
 		this.ce = ce;
 	}
 	
 	public String Info() {
-		return ("Al�ada maxima "+ alsada);
+		return ("Al�ada maxima "+ alsada);
 	}
 
 }
--- src/barri/RDistancia.java
+++ src/barri/RDistancia.java
@@ -7,6 +7,13 @@ public abstract class RDistancia extends RestriccioBarris implements RMax, REspa
 	int dist;
 	boolean max;
 
+        /**
+         * Crea una instancia de la restricció de distancia.
+         * @param ID Identificador de la distancia.
+         * @param d Distancia que aplicara la restricció.
+         * @param m Indica si la distancia es mínima o màxima.
+         * @param e Espai sobre el que s'aplicarà la restricció.
+         */
 	public RDistancia(int ID, int d, boolean m, Espai e) {
 		super(ID);
 		dist = d;
@@ -15,6 +22,14 @@ public abstract class RDistancia extends RestriccioBarris implements RMax, REspa
 
 	}
 
+        /**
+         * Calcula la distancia entre dos coordenades.
+         * @param x1 Coordenada X del primer element.
+         * @param y1 Coordenada Y del primer element.
+         * @param x2 Coordenada X del segon element.
+         * @param y2 Coordenada Y del segon element.
+         * @return La distancia entre les dos coordenades.
+         */
 	protected int dist(int x1, int y1, int x2, int y2) {
 		int x, y;
 		x = x1 - x2;
@@ -24,43 +39,64 @@ public abstract class RDistancia extends RestriccioBarris implements RMax, REspa
 		if (y < 0)
 			y = y * (-1);
 
-		// int d = (int)Math.sqrt(x*x + y*y);
 		int d = (x + y);
 		return d;
 	}
 
+        /**
+         * Calcula la distancia entre dos coordenades.
+         * @param x1 Coordenada X del primer element.
+         * @param y1 Coordenada Y del primer element.
+         * @param x2 Coordenada X del segon element.
+         * @param y2 Coordenada Y del segon element.
+         * @return La distancia entre les dos coordenades.
+         */
 	protected double distd(int x1, int y1, int x2, int y2) {
 		int x, y;
 		x = x1 - x2;
-		// if (x < 0) x = x*(-1);
 		y = y1 - y2;
-		// if (y < 0) y = y*(-1);
 
 		double d = Math.sqrt(x * x + y * y);
 		return d;
 	}
 	
-	
+	/**
+         * Consulta la distancia assignada a la restricció. 
+         * @return La distancia assignada a la restricció.
+         */
 	public int ConsultarDist() {
 		return dist;
 	}
 	
+        /**
+         * Modifica la distancia assignada a la restricció.
+         * @param d Nova distancia assignada a la restricció.
+         */
 	public void ModificarDist(int d) {
 		dist = d;
 	}
 	
+        /**
+         * Consulta si la distancia assignada es màxima o mínima.
+         * @return Cert si la distancia assignada es màxima.
+         */
 	public boolean EsMax() {
 		return max;
 	}
 	
+        /**
+         * Modifica si la distancia assignada es màxima o mínima.
+         * @param m Indica si la distancia assignada es màxima o mínima.
+         */
 	public void CanviaMax(boolean m) {
 		max = m;
 	}
 	
+        /**
+         * Assigna l'espai on s'aplicarà la restrició.
+         * @param e Espai on s'aplicarà la restricció.
+         */
 	public void AssignaEspai(Espai e) {
 		this.e = e;
 	}
-	
-
-
 }
